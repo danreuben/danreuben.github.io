d8c68123ce2069be18fc05ce37f666fb6389c771
danreuben <stuffthatsthere@gmail.com>
2022-11-27 09:58:19 -0500

    added navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy({'_src/robots.txt': '/robots.txt' });
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
     return {
         dir: {
