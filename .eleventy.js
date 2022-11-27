c06efdc7e9b2a82da0eca2370056c4921c5808f5
danreuben <stuffthatsthere@gmail.com>
2022-11-27 13:11:30 -0500

    added concrete and normalize css

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);  
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPassthroughCopy({"node_modules/concrete.css/concrete.css": "css/concrete.css"});
+  eleventyConfig.addPassthroughCopy({"node_modules/normalize.css/normalize.css": "css/normalize.css"});
   eleventyConfig.addPassthroughCopy("_src/css");
   eleventyConfig.addPassthroughCopy("_src/img");
   eleventyConfig.addPassthroughCopy("_src/js");
-  eleventyConfig.addPassthroughCopy('_src/robots.txt');    
+  eleventyConfig.addPassthroughCopy("_src/robots.txt");    
 
   return {
       dir: {
--- _src/index.md
+++ _src/index.md
@@ -4,22 +4,10 @@ layout: base
 eleventyNavigation:
   key: home
   order: 1
-pagination:
-  data: collections.post
-  size: 3
-  reverse: true
-  alias: posts
 ---
 
 # posts
 
-{%- for post in posts %}
-- [{{ post.data.title }}]({{ post.url }})
+{%- for post in collections.post %}
+* [{{ post.data.title }}]({{ post.url }})
 {%- endfor %}
-
-<!-- {% if pagination.href.previous %}
-  <a href="{{pagination.href.previous}}">Previous Page</a>
-{% endif %}
-{% if pagination.href.next %}
-  <a href="{{pagination.href.next}}">Next Page</a>
-{% endif %} -->
